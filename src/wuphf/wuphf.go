9bb70e227c25bbfef360e0f437e9d3b479208c4b
Sudarshan <sudar.theone@gmail.com>
2016-02-14 20:43:19 +0530

    Changed to struct format

--- src/wuphf/wuphf.go
+++ src/wuphf/wuphf.go
@@ -8,23 +8,30 @@ import (
 )
 
 const (
-	accountSid = "afasdffdgfsdfsfs"
-	authToken = "asdfafdafdfsdfsf"
+	accountSid = "ACff2b52113ad171f7aa2976c8a3f55f7c"
+	authToken = "abb129ba34817882b26af8c7324508fb"
+	twiloPhone = "+17722176343" 
 	urlStr =  "https://api.twilio.com/2010-04-01/Accounts/" + 
 				accountSid + "/Messages.json"
 )
 
-func main(){
+type wuphfMaster struct {
+	facebook string
+	googlePlus string
+}
+
+func (wuphf wuphfMaster) sendSMS(phoneNumber , body string) bool {
 	v := url.Values{}
-	v.Set("To" , "123145124134")
-	v.Set("From" ,"124124144141" )
-	v.Set("Body", "Wuphf!")
+	v.Set("To" , phoneNumber)
+	v.Set("From" , twiloPhone)
+	v.Set("Body", body)
 	rb := *strings.NewReader(v.Encode())
 
 	client := &http.Client{}
 	req , err := http.NewRequest("POST", urlStr, &rb)
 	if err != nil {
 		fmt.Println("ERROR: " , err)	
+		return false
 	}else{
 		req.SetBasicAuth(accountSid, authToken)
 		req.Header.Add("Accept", "application/json")
@@ -32,10 +39,16 @@ func main(){
 		resp, err := client.Do(req)
 		if err != nil{
 			fmt.Println("ERROR: ", err)	
+			return false
 		}else{
 			fmt.Println("Success: " , resp.Status)	
 		}
 	}
-
-
+	return true
 }
+
+func main() {
+	wuf := wuphfMaster{"fb", "gp"}
+	wuf.sendSMS("+919789099524" , "WUPHF!!!")
+}
+
